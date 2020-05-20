348ea597864abb004338b578edb972b13fea3219
khcho902 <khcho902@gmail.com>
2020-05-21 05:37:32 +0900

    start springboot project

---
+++ src/main/java/com/khcho902/gangnamspringbootstudy/GangnamSpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.khcho902.gangnamspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangnamSpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangnamSpringbootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khcho902/gangnamspringbootstudy/GangnamSpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khcho902.gangnamspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangnamSpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
