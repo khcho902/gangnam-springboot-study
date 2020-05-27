d2a77ba2f68ec59bb8e0d0d8751cd8d0adf10763
khcho902 <khcho902@gmail.com>
2020-05-28 01:18:53 +0900

    applied connection_pool to RestTemplate

--- src/main/java/com/khcho902/gangnamspringbootstudy/config/RestTemplateConfig.java
+++ src/main/java/com/khcho902/gangnamspringbootstudy/config/RestTemplateConfig.java
@@ -1,7 +1,10 @@
 package com.khcho902.gangnamspringbootstudy.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +12,15 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(3000);
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(5)
+                .build();
+        factory.setHttpClient(httpClient);
+
+        return new RestTemplate(factory);
     }
 }
